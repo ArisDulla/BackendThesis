9dcbca61ee01afaec21e4f490d3fe517fee24aa6
arisdulla <it21964@hua.gr>
2024-02-08 17:34:53 +0000

    Address management: Implemented the ViewSet, Serializer, Test and Url

--- passportBackend/settings.py
+++ passportBackend/settings.py
@@ -40,7 +40,15 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'polls.apps.PollsConfig',
+    'rest_framework',
 ]
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
---
+++ polls/viewsAll/v1_AddressViewSet.py
@@ -0,0 +1,61 @@
+from rest_framework import viewsets
+from rest_framework.response import Response
+from ..models import Address
+from ..serializers.s1_AddressSerializer import AddressSerializer
+from rest_framework import status
+
+class AddressViewSet(viewsets.ModelViewSet):
+    queryset = Address.objects.all()
+    serializer_class = AddressSerializer
+    format_kwarg = None 
+
+    def initial(self, request, *args, **kwargs):
+        """
+        Perform any necessary initializations.
+        """
+        # Your initialization logic here
+        pass
+
+    def list(self, request, *args, **kwargs):
+        """
+        Return a list of all addresses.
+        """
+        
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def create(self, request, *args, **kwargs):
+        """
+        Create a new address.
+        """
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def retrieve(self, request, *args, **kwargs):
+        """
+        Retrieve a single address instance.
+        """
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
+
+    def update(self, request, *args, **kwargs):
+        """
+        Update an existing address.
+        """
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response(serializer.data)
+
+    def destroy(self, request, *args, **kwargs):
+        """
+        Delete an existing address.
+        """
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
