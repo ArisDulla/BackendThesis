ce461db182dea2e7a7c491c3f0aba6e4681780de
arisdulla <it21964@hua.gr>
2024-02-09 18:33:14 +0000

    Add IsAdminUser permission for AddressViewSet

--- polls/viewsAll/v1_AddressViewSet.py
+++ polls/viewsAll/v1_AddressViewSet.py
@@ -3,11 +3,13 @@ from rest_framework.response import Response
 from ..models import Address
 from ..serializers.s1_AddressSerializer import AddressSerializer
 from rest_framework import status
+from rest_framework.permissions import IsAdminUser
 
 class AddressViewSet(viewsets.ModelViewSet):
     queryset = Address.objects.all()
     serializer_class = AddressSerializer
     format_kwarg = None 
+    permission_classes = [IsAdminUser]
 
     def initial(self, request, *args, **kwargs):
         """
