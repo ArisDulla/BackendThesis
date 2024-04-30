bf1e8466f6827b681337f55a678194023acc9a70
arisdulla <it21964@hua.gr>
2024-04-30 22:33:37 +0000

    Edit permission for employee's department and remove processors

--- polls/permissions/p1_AddressPermissions/p1_isAdminOrIsSelfAddress.py
+++ polls/permissions/p1_AddressPermissions/p1_isAdminOrIsSelfAddress.py
@@ -1,5 +1,6 @@
 from rest_framework import permissions
 from ...models import UserAddress
+from django.shortcuts import get_object_or_404
 
 
 class IsAdminOrIsSelfAddress(permissions.BasePermission):
@@ -10,7 +11,6 @@ class IsAdminOrIsSelfAddress(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
 
         user = request.user
-
         #
         # ADMIN
         #
@@ -21,8 +21,40 @@ class IsAdminOrIsSelfAddress(permissions.BasePermission):
             return True
 
         #
+        # Employee
+        #
+        # Check if the employee's department matches the department of the object(address of cityzen)
+        #
+        if hasattr(user, "employee"):
+
+            employee = user.employee
+
+            try:
+                citizen = get_object_or_404(UserAddress, address=obj)
+            except Exception:
+                citizen = None
+
+            usercx = getattr(citizen, "user", None)
+            cityzens = getattr(usercx, "cityzens", None)
+            departmentxx = getattr(cityzens, "department", None)
+
+            if departmentxx is not None:
+
+                if employee.department == departmentxx:
+
+                    if employee.employee_type in ["YP02", "YP01", "SEC"]:
+
+                        return True
+
+        #
         # Allow object's owner to access.
         #
-        user_addresses = UserAddress.objects.filter(user=user, address=obj)
+        try:
+            user_addresses = get_object_or_404(UserAddress, user=user, address=obj)
+        except Exception:
+            return False
 
-        return user_addresses.exists()
+        if user_addresses:
+            return True
+
+        return False
--- polls/permissions/p2_PhoneNumberPermissions/p1_isAdminOrIsSelfPhoneNumber.py
+++ polls/permissions/p2_PhoneNumberPermissions/p1_isAdminOrIsSelfPhoneNumber.py
@@ -1,5 +1,6 @@
 from rest_framework import permissions
 from ...models import UserPhoneNumber
+from django.shortcuts import get_object_or_404
 
 
 class IsAdminOrIsSelfNumber(permissions.BasePermission):
@@ -10,7 +11,6 @@ class IsAdminOrIsSelfNumber(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
 
         user = request.user
-
         #
         # ADMIN
         #
@@ -21,8 +21,42 @@ class IsAdminOrIsSelfNumber(permissions.BasePermission):
             return True
 
         #
+        # Employee
+        #
+        # Check if the employee's department matches the department of the object(phoneNumber of cityzen)
+        #
+        if hasattr(user, "employee"):
+
+            employee = user.employee
+
+            try:
+                citizen = get_object_or_404(UserPhoneNumber, phoneNumber=obj)
+            except Exception:
+                citizen = None
+
+            usercx = getattr(citizen, "user", None)
+            cityzens = getattr(usercx, "cityzens", None)
+            departmentxx = getattr(cityzens, "department", None)
+
+            if departmentxx is not None:
+
+                if employee.department == departmentxx:
+
+                    if employee.employee_type in ["YP02", "YP01", "SEC"]:
+
+                        return True
+
+        #
         # Allow object's owner to access.
         #
-        user_phoneNumber = UserPhoneNumber.objects.filter(user=user, phoneNumber=obj)
+        try:
+            user_phoneNumber = get_object_or_404(
+                UserPhoneNumber, user=user, phoneNumber=obj
+            )
+        except Exception:
+            return False
 
-        return user_phoneNumber.exists()
+        if user_phoneNumber:
+            return True
+
+        return False
