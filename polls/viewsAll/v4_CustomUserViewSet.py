6f54f2882a364bf114b1bb78ef464b64fa61c9fb
arisdulla <it21964@hua.gr>
2024-05-26 17:44:21 +0000

    Create get_role_user action

--- polls/viewsAll/v4_CustomUserViewSet.py
+++ polls/viewsAll/v4_CustomUserViewSet.py
@@ -84,3 +84,42 @@ class CustomUserViewSet(viewsets.ModelViewSet):
             },
             status=200,
         )
+
+    #
+    # Action get_role_user about -- Decide which dashboard to display in front based on the user's role --
+    #
+    @action(detail=False, methods=["get"], permission_classes=[IsAuthenticated])
+    def get_role_user(self, request):
+
+        user = request.user
+        type_user = "NONE"
+
+        #
+        # FOR CITYZENS
+        #
+        if hasattr(user, "cityzens"):
+            type_user = "cityzen"
+
+        #
+        # FOR Employee
+        #
+        elif hasattr(user, "employee"):
+            employee = user.employee
+            type = employee.employee_type
+
+            if type == "YP01":
+                type_user = "employeeYP01"
+
+            elif type == "YP02":
+                type_user = "employeeYP02"
+
+            elif type == "SEC":
+                type_user = "employeeSEC"
+
+        #
+        # FOR ADMIN
+        #
+        elif user.is_admin:
+            type_user = "admin"
+
+        return Response({"type_user": type_user}, status=200)
