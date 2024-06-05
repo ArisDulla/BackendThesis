643fcfdad1ea9a76d68ecf17f478973d1c8f0b27
arisdulla <it21964@hua.gr>
2024-06-05 13:50:48 +0000

    Set DEBUG = False

--- passportBackend/settings.py
+++ passportBackend/settings.py
@@ -27,7 +27,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "uploads")
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = config("ALLOWED_HOSTS").split(",")
 
